b2a9b18b1dfeec67026f6979bb6517536cdbf05f
HydraSpex <info@hydraspex.com>
2022-04-21 19:52:00 +0200

    v2.0.0
    Added SPE2.x

--- SPE_Handler.py
+++ SPE_Handler.py
@@ -5,14 +5,28 @@ import math
 import os
 import time
 
+#Reading Bytes
 def from_bytes(b, format, offset):
     calcsize = struct.calcsize(format)
     return struct.unpack(format, b[offset:offset+calcsize])[0]
 
+#Reading the XML Line of SPE3.x Files
 def openXMLline(filename):
     num_lines = sum(1 for line in open(filename, "rb"))
     f = open(filename, "rb")
     lines = f.readlines()
+    
+    Txt_Dai = open("Dai_new.txt", "w")  
+
+    f = open(filename, "rb")
+    lines = f.readlines()
+
+    i = 0
+    while i < num_lines:
+        Txt_Dai.write(str(lines[i]) + "\n")
+        i += 1
+    Txt_Dai.close()
+
     XMLline = str(lines[num_lines-1])
     startXML = XMLline.find("<SpeFormat")
     XMLline = XMLline[startXML:]
@@ -57,6 +71,7 @@ def openXMLline(filename):
     Wavedata = []
     WavedataRound = []
     i = 0
+    
     while i < int(Width):
         PosNext = WaveData.find(",")
         Wavedata.append(WaveData[:PosNext])
@@ -74,23 +89,31 @@ def openXMLline(filename):
     CWLData = XMLline[PosCWL:PosCWLEnd]
     CWL = CWLData[(CWLData.find("\">")+2):]
 
-    return Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Wavedata, WavedataRound
+    PosBG = (XMLline.find("<BackgroundCorrection><Enabled") + 25)
+    BGData = XMLline[PosBG:]
+    PosBGBegin = (BGData.find(">")+1)
+    PosBGEnd = BGData.find("<")
+    BG = BGData[PosBGBegin:PosBGEnd]
 
+    PosGrating = XMLline.find("<Grating><Selected")
+    GratingData = XMLline[PosGrating:]
+    PosGratingBegin = GratingData.find("[")
+    PosGratingEnd = (GratingData.find("]")+1)
+    Grating = GratingData[PosGratingBegin:PosGratingEnd]
+
+    return Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound
+
+#Converting the SPE-Data to TXT-File.
+#If space!="tab" the spacer between the values will be ";".
+#If header=False the TXT-File will not contain the important setup information.
+#If invert=True the TXT-File will be inverted.
 def convert_txt(filename, FolderName, space="tab", header=True, invert=False):
+    print("Converting...")
     File = FolderName + "/SpectraNew.txt"
-    Txt_Point = open(File, "w")                                                                                              #Erstellt und öffnet eine neue Datei im Schreibmodus
+    Txt_Point = open(File, "w") 
 
-    Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Wavedata, WavedataRound = openXMLline(filename)
+    np_type, itemsize, Count, Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound = getData(filename)
 
-    print("SPE version: ", Version)
-    print("Frame width: ", Width)
-    print("Frame height: ", Height)
-    print("Number of frames: ", Frame)
-    print("Exposure (ms): ", ExpTime)
-    print("Date collected: ", Date)
-    print("Time collected (hhmmss): ", Time)
-    print("Laser Wavelength (nm): ", Laser)
-    print("Central Wavelength (nm): ", CWL)
     if header==True:
         Txt_Point.write("SPE version: " + str(Version) + "\n")
         Txt_Point.write("Frame width: " + str(Width) + "\n")
@@ -148,76 +171,211 @@ def convert_txt(filename, FolderName, space="tab", header=True, invert=False):
 
         if space=="tab":
             for i in range(0, Width):
-                Txt_Point.write(Wavedata[i])
+                Txt_Point.write(str(Wavedata[i]))
                 for j in range(0, Frame):
                     Txt_Point.write("\t" + str(data[j][i]))
                 Txt_Point.write("\n")
         else:
             for i in range(0, Width):
-                Txt_Point.write(Wavedata[i])
+                Txt_Point.write(str(Wavedata[i]))
                 for j in range(0, Frame):
                     Txt_Point.write("; " + str(data[j][i]))
                 Txt_Point.write("\n")
 
     Txt_Point.close()
     file.close()
-    print("done")
 
-def spectralMap_integral(filename, FolderName, spectralMap=True, singleSpectra=False):
-    
-    Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Wavedata, WavedataRound = openXMLline(filename)
-
+#Getting all the important setup informations out of the SPE-Header (SPE2.x), or the XML-Footer (SPE3.x) 
+def getData(filename):
     file = open(filename, "rb")
     bytes = file.read()
+
+    SPEVersion = round(from_bytes(bytes, "f", 1992),1)
 
     datatype = from_bytes(bytes, "h", 108)
     frame_width = from_bytes(bytes, "H", 42)
     frame_height = from_bytes(bytes, "H", 656)
     num_frames = from_bytes(bytes, "i", 1446)
-    
-    print("datatype:", datatype)
     to_np_type = [np.float32, np.int32, np.int16, np.uint16, None, np.float64, np.uint8, None, np.uint32]
     np_type = to_np_type[datatype]
     itemsize = np.dtype(np_type).itemsize
-    print("itemsize: ", itemsize)
-    print("numpy type:", np_type)
-    
-    count = frame_width * frame_height
-    Px = int(math.sqrt(num_frames))
+    XMLOffset = from_bytes(bytes, "64Q", 678)
+
+    Count = frame_width * frame_height
+
+    if SPEVersion >= 3:
+        try:
+            Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound = openXMLline(filename)
+        except:
+            pass
+    else:
+        Version = SPEVersion
+        Frame = num_frames
+        Width = frame_width
+        Height = frame_height
+        Laser = from_bytes(bytes, "d", 3311)
+        LocalDate = from_bytes(bytes, "16s", 20)
+        LocalDate = LocalDate.decode("utf-8", "ignore")
+        Day = LocalDate[:2]
+        Year = LocalDate[5:9]
+        Month = LocalDate[2:5]
+        if Month == "Jan":
+            Month = "01"
+        elif Month == "Feb":
+            Month = "02"
+        elif Month == "Mar":
+            Month = "03"
+        elif Month == "Apr":
+            Month = "04"
+        elif Month == "May":
+            Month = "05"
+        elif Month == "Jun":
+            Month = "06"
+        elif Month == "Jul":
+            Month = "07"
+        elif Month == "Aug":
+            Month = "08"
+        elif Month == "Sep":
+            Month = "09"
+        elif Month == "Oct":
+            Month = "10"
+        elif Month == "Nov":
+            Month = "11"
+        elif Month == "Dec":
+            Month = "12"
+        else:
+            Month = "00"
+        Date = Year + "-" + Month + "-" + Day
+        LocalTime = from_bytes(bytes, "6s", 172)
+        LocalTime = LocalTime.decode("utf-8", "ignore")
+        Time = LocalTime[:2] + ":" + LocalTime[2:4] + ":" + LocalTime[4:]
+        UTCTime = from_bytes(bytes, "6s", 179)
+        UTCTime = UTCTime.decode("utf-8", "ignore")
+        ExpTime = from_bytes(bytes, "f", 10)
+        CWL = from_bytes(bytes, "f", 72)
+        Grating = from_bytes(bytes, "32f", 650)
+        BG = from_bytes(bytes, "i", 150)
+        XStartNM = from_bytes(bytes, "d", 3183)
+        XStopNM = from_bytes(bytes, "d", 3199)
+
+        PXSize = (XStopNM-XStartNM)/(Count-1)
+        Wavedata = []
+        WavedataRound = []
+        j = 0
+        while j < Count:
+            val = XStartNM + (j * PXSize)
+            Wavedata.append(val)
+            WavedataRound.append(round(val, 2))
+            j += 1
+
+    return np_type, itemsize, Count, Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound
+
+#Plots all Spectra in the file as 2D-Plot
+def singleSpectra_plot(filename, FolderName):
+    np_type, Itemsize, Count, Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound = getData(filename)
+
+    print("\n------------- Important Informations -------------")
+    print("SPE-Version:", Version)
+    print("Frames:", Frame)
+    print("Width:", Width)
+    print("Height:", Height)
+    print("Laser Wavelength (nm):", Laser)
+    print("Date:", Date)
+    print("Local Time:", Time)
+    print("Exposuretime (s):", ExpTime)
+    print("Central Wavelength (nm):", CWL)
+    print("Grating:", Grating)
+    print("BG:", BG)
+    print("------------- End -------------\n")
+
+    Frame = int(Frame)
+    Itemsize = int(Itemsize)
+    Count = int(Count)
+
+    file = open(filename, "rb")
+    bytes = file.read()
+
+    Px = int(math.sqrt(int(Frame)))
+    for i in range(0, Frame):
+        print("Processing frame number ", i+1)
+        offset = 4100 + i*Count*Itemsize
+        data = np.frombuffer(bytes, dtype=np_type, count=Count, offset=offset)
+        File = FolderName + "/Frame_" + str((i+1)) + ".png"
+        plt.figure()
+        plt.plot(WavedataRound, data)
+        #plt.plot(data)
+        plt.savefig(File, dpi=300)
+        plt.close()
+
+#Integrates all Spectra in the File and Plots a Map of it
+def spectralMap_integral(filename, FolderName):
+
+    np_type, Itemsize, Count, Version, Frame, Width, Height, Laser, Date, Time, ExpTime, CWL, Grating, BG, Wavedata, WavedataRound = getData(filename)
+
+    print("\n------------- Important Informations -------------")
+    print("SPE-Version:", Version)
+    print("Frames:", Frame)
+    print("Width:", Width)
+    print("Height:", Height)
+    print("Laser Wavelength (nm):", Laser)
+    print("Date:", Date)
+    print("Local Time:", Time)
+    print("Exposuretime (s):", ExpTime)
+    print("Central Wavelength (nm):", CWL)
+    print("Grating:", Grating)
+    print("BG:", BG)
+    print("------------- End -------------\n")
+
+    Frame = int(Frame)
+    Itemsize = int(Itemsize)
+    Count = int(Count)
+
+    file = open(filename, "rb")
+    bytes = file.read()
+
+    Px = int(math.sqrt(int(Frame)))
     Integral = []
-    for i in range(0, num_frames):
-        print("Showing frame number ", i+1)
-        data = np.frombuffer(bytes, dtype=np_type, count=count, offset=4100 + i*count*itemsize)
+    for i in range(0, Frame):
+        print("Processing frame number ", i+1)
+        offset = 4100 + i*Count*Itemsize
+        data = np.frombuffer(bytes, dtype=np_type, count=Count, offset=offset)
         Summe = sum(data)
         Integral.append(Summe)
-        if singleSpectra == True:
-            File = FolderName + "/Frame_" + str((i+1)) + ".png"
-            plt.figure()
-            plt.plot(WavedataRound, data)
-            plt.savefig(File, dpi=300)
-            plt.close()
-    if spectralMap == True:
-        try:
-            File = FolderName + "/SpectralMap.png"
-            dataInt = np.reshape(Integral, (Px,Px))
-            plt.figure()
-            plt.imshow(dataInt)
-            plt.show()
-            plt.close()
-        except Exception as e: print(e)
+
+    try:
+        File = FolderName + "/SpectralMap.png"
+        dataInt = np.reshape(Integral, (Px,Px))
+        plt.figure()
+        plt.imshow(dataInt)
+        plt.show()
+        plt.close()
+        plt.figure()
+        plt.imshow(dataInt)
+        plt.savefig(File)
+        plt.close()
+    except Exception as e: print(e)
+
     file.close()
 
-def spectra_from_spe(FileName, spectralMap=True, singleSpectra=True, convert=True, space="tab", header=True, invert=True):
+
+def spectra_from_spe(FileName, spectralMap=False, singleSpectra=True, convert=True, space="tab", header=True, invert=True):
     print("Filename:", FileName)
+
+    #Create new Folder, if the File is converted or spectra are plotted
     FolderName = time.strftime("Spectra_%Y-%m-%d_%H-%M-%S")
-    if singleSpectra == True or convert == True:
-        try:
-            os.makedirs(FolderName)
-            print("Data folder created")
-        except:
-            print("Data-Folder already exist")
+    try:
+        os.makedirs(FolderName)
+        print("Data folder created")
+    except:
+        print("Data-Folder already exist")
     
-    spectralMap_integral(FileName, FolderName, spectralMap=spectralMap, singleSpectra=singleSpectra)
+    if singleSpectra == True:
+        singleSpectra_plot(FileName, FolderName)
+
+    if spectralMap == True:
+        spectralMap_integral(FileName, FolderName)
     
     if convert == True:
         convert_txt(FileName, FolderName, space=space, header=header, invert=invert)
+
+    print("done")
--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 import SPE_Handler as SPE
 
-SPE.spectra_from_spe("Testspectra/Testfile_SpectralMap.spe", invert=False, singleSpectra=False)
-SPE.spectra_from_spe("Testspectra/Testfile_Single.spe", spectralMap=False)
+SPE.spectra_from_spe("Testspectra/Testfile_SpectralMap.spe", spectralMap=True, singleSpectra=False, invert=False)
+SPE.spectra_from_spe("Testspectra/Testfile_Single_SPE3.spe", spectralMap=False)
+SPE.spectra_from_spe("Testspectra/Testfile_Single_SPE2.spe", spectralMap=False)
